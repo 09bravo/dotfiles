ad1c491461db324fc4511d1c4da49fafa8526598
09bravo <157029034+09bravo@users.noreply.github.com>
2025-11-26 13:29:31 +0000

    Removed Unnecessary sudo

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 su -c 'pacman -S swaybg sway foot ttf-nerd-fonts-symbols ttf-fira-code dmenu'
-su -c 'sudo mv bin/* /bin/'
+su -c 'mv bin/* /bin/'
 rm -Rf wall-e/sources.txt
 mkdir -p ~/.config
 mv sway/ foot/ ~/.config/
