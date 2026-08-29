5134ba2ea3dc10afb029d2a0cee71e1fff0a8586
09bravo <hussin.20062009@gmail.com>
2026-08-30 01:23:23 +0300

    Updated config and improved installer

--- setup.sh
+++ setup.sh
@@ -1,10 +1,26 @@
-if command -v sudo &>/dev/null
-then
-sudo pacman -S grim flameshot ttf-dejavu swaybg sway foot ttf-nerd-fonts-symbols ttf-fira-code wofi autotiling-rs xdg-desktop-portal-wlr
-else
-su -c 'pacman -S grim flameshot ttf-dejavu swaybg sway foot ttf-nerd-fonts-symbols ttf-fira-code wofi autotiling-rs xdg-desktop-portal-wlr'
+#!/bin/bash
+set -e
+
+echo "This script assumes that this is a fresh install of Arch/Artix based Distros."
+read -rp "Continue? [y/n] " answer
+if [ "$answer" != "y" ]; then
+	echo "Bye!"
+	exit 0
 fi
+
+packages="grim flameshot ttf-dejavu noto-fonts noto-fonts-emoji noto-fonts-cjk swaybg sway foot ttf-nerd-fonts-symbols ttf-fira-code wofi autotiling-rs xdg-desktop-portal-wlr xorg-xwayland xdg-desktop-portal-gtk"
+if command -v sudo &>/dev/null; then
+	sudo pacman -S --needed $packages
+	sudo cp startw /bin/startw
+elif command -v doas &>/dev/null; then
+	doas pacman -S --needed $packages
+	doas cp startw /bin/startw
+else
+	su -c "pacman -S --needed $packages"
+	su -c "cp startw /bin/startw"
+fi
+
 mkdir -p ~/.config
 cp -r flameshot sway/ foot/ wofi/ ~/.config/
 cp -r wall-e ~/
-rm -Rf ~/wall-e/sources.txt
+printf "Finished!\nYou should now start sway with startw.\nstartw is a simple script which adds some environment variables and starts sway with D-Bus.\nMake sure D-Bus is installed and enabled.\n"
